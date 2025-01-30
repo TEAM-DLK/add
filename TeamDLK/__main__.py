72080fca44c4678f48d6955549ce8b6152e8d82c
達南佳亞 <93440107+TEAM-DLK@users.noreply.github.com>
2025-01-30 21:50:36 +0530

    Update __main__.py

--- TeamDLK/__main__.py
+++ TeamDLK/__main__.py
@@ -41,7 +41,7 @@ async def track_new_members(update: Update, context: CallbackContext) -> None:
                     can_send_messages=True,
                     can_send_media_messages=True,
                     can_send_other_messages=True,
-                    can_add_web_page_previews=True,
+                    can_add_web_page_previews=False,
                     can_send_polls=True,
                     can_invite_users=True,
                     can_pin_messages=False,
